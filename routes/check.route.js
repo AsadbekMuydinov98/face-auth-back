1170241d45fc983f732e09d1085c65c563560293
Asadbek <Asadbek@gmail.com>
2024-10-21 13:00:52 +0900

    deleted

--- routes/check.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const checkController = require('../controllers/check.controller');
-
-router.post('/check-spelling', checkController.checkSpelling);
-router.post('/save-spell-check', auth, checkController.saveSpellCheck);
-
-module.exports = router;
